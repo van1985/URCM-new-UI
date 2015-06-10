a9cf238347ad4c219a9b0344a08394ede9d616cf
Leandro García Vandenbosch <vanden@Leandros-MacBook-Pro.local>
2015-06-10 02:03:27 -0300

    Add Disable selection/copy in UIWebView && directive for touch events

---
+++ app/scripts/directives/touchEvent.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('Directives').directive('touchEvent',
+    function() {
+        return {
+            scope: false,
+            restrict: 'A',
+            replace:false,
+            link: function(scope,element, attrs){
+
+                element.on('touchstart', function(event) {
+                  element.addClass(attrs.touchStartClass);
+                });
+
+                element.on('touchend', function(event){
+                    element.removeClass(attrs.touchEndClass);
+                });
+            }
+        }
+});  
